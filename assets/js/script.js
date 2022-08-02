9651c80e0eb81a09ee96d023a8b04539cb3bb82a
KathiaBernal <kathiabernal@gmail.com>
2022-08-01 22:20:34 -0600

    Ejercicios realizados en JavaScript. Mis primeros pasos.

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+//Este es un comentario de una sola línea usando dos barras o diagonales
+/*Este es un
+comentario multilinea usando
+una diagonal y asterisco para abrir
+y asterisco diagonal para cerrar*/
+
+/* */
+
+//Una alerta es una ventana de diálogo que muestra un mensaje
+// alert("Hola Kathia, estoy vivo");
+
+// Pedimos la información al usuario con un prompt, la guardamos en una variable llamada nombre para después mostrarlo
+nombre = prompt("¿Cómo te llamas?");
+
+// Usamos un console.log para mostrar lo que se guardó en la variable nombre
+console.log(nombre);
+
+//JS es case sensitive, por lo tanto si escribimos "Hola" y "hola" tendremos dos cosas completamente diferentes.
+// nombre = "Juan"
+// Nombre = "juan"
+
+// Comida Favorita de Felipe
+// Comida-Favorita-de-Felipe
+// comidaFavoritaDeFelipe
+// laComidaFavoritaDeFelipeQueComeTodosLosLunesALasOchoDeLAMañana
+
+// comidaFavorita
+
+// dato1
+// dato2
+// dato3
+
+//Programa para calcular el área de un tiángulo
+//Definimos dos variables llamadas base y altura, donde al valor a tomar en cuenta para la fórmula es definido por el usuario o a través de un propet
+base = prompt("Ingresa la base del triángulo");
+altura = prompt("Ingresa la altura del tríangulo");
+areaTriangulo = (base * altura)/2;
+
+//Creamos una fórmula que utilice los datos ingresados
+alert("areaTriangulo");
+//Concatenación de mensaje + resultado
+console.log ("El área del Triángulo es: " + areaTriangulo);
+
+
+
+//CamelCase
+
+//prompt sería como input en python
+//console.log sería como print en python
---
+++ assets/js/variables.js
@@ -0,0 +1,65 @@
+// Mejores prácticas:
+
+// camelCase
+// Utilizar mayúsculas en la mitad de nuestras variables
+// variableTriangulo, miNombre, miEdad, miDireccion
+
+// Palabras reservadas
+// No usar palabras reservadas para declarar una variable
+// if, return, true, false, for, while, let, var, etc...
+
+var while;
+var null ;
+var if;
+var true;
+var false;
+var var;
+var const;
+var let;
+
+// Seguir un standard personal
+
+// Pueden contener número, letras, guión bajo, símbolo de dólar
+var a;
+var a1;
+var a_Nombre;
+var a$Direccion;
+
+// Declaración de variables
+// Type number
+var edad = 26;
+var peso = 63;
+var miopia = 3;
+var astigmatismo = 3;
+
+// Type boolean
+var ciego = true;
+var hambre = true;
+var bateria = false;
+var ia = true;
+
+// Type string
+var nombre = "Kathia Bernal";
+var ciudad = "Tepic";
+var nacionalidad = "Mexicana";
+
+//Type null
+var ganasDeBibir = null;
+var nineros = null;
+var briAmonestaciones = null;
+
+// Variables declaradas con un mismo valor
+var x, y, z = 4;
+// Variables undefined
+var xx, yy, zz;
+
+// Variable tres = 4
+// var dos = tres (4)
+// var uno = dos (4)
+var uno = dos = tres = 4;
+
+// Escribir en una sola linea variables undefined de nuestro carrito de compras
+var catsum, leche, galletas, cereal, rollo, jabon, shampoo, detergente
+
+
+
